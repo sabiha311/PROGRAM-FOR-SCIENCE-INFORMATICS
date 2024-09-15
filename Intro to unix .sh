777c5a233b995780ac00a7e2514099b131e81db7
sabiha311 <sabiha.sk1999@gmail.com>
2024-09-15 13:05:25 -0400

    Update Intro to unix .sh

--- Intro to unix .sh
+++ Intro to unix .sh
@@ -1,1 +1,18 @@
+#creating users home directory
+cd~
+#create directory titled informatics_573
+mkdir informatics_573
+cd informatics_573
+#download all secondary assemblies for human chromosome 1
+wget -r-l1-nd-A"CHR1_*" -R"chr1.fa.gz"
+#unzip all downloaded chromosome1 assemblies
+gunzip chr1_*
+#creating new empty file called "data_summary.txt:"
+touch data_summary.txt
+#append detailed information about each file
+ls -lh >> data_summary.txt
+#append first 10 lines of each assembly
+head -n 10 chr1_* >> data_summary.txt
+#append the name of each assembly and total number of files
+wc chr1_* >> data_summary.txt
 
